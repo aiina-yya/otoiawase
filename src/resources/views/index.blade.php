1197ef4d7913e38d0e8a9223e7cf8c7d0323e1f3
aiina <aina19920331@gmail.com>
2026-03-11 01:59:48 +0900

    app,index.blade

---
+++ src/resources/views/index.blade.php
@@ -0,0 +1,141 @@
+extends('layouts.app')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/index.css') }}">
+@endsection
+
+@section('content')
+<div class="contact-form__content">
+    <div class="contact-form__heading">
+        <h2>Contact</h2>
+    </div>
+    <form class="form" action="">
+        <div class="form__group">
+            <div class="form__group-title">
+                <span class="form__label--item">お名前</span>
+                <span class="form__label--required">※</span>
+            </div>
+            <div class="form__group-content">
+                <div class="form__input--text">
+                    <input type="text" name="last_name" placeholder="例：山田" old />
+                    <input type="text" name="first_name" placeholder="例：太郎" old />
+                </div>
+                <div class="form__error">
+                    <!--バリデーション-->
+                </div>
+            </div>
+        </div>
+        <div class="form__group">
+            <div class="form__group-title">
+                <span class="form__label--item">性別</span>
+                <span class="form__label--required">※</span>
+            </div>
+            <div class="form__group-content">
+                <div class="form__input--radio">
+                    <input type="radio" name="gender" value="man" old />男性
+                    <input type="radio" name="gender" value="woman"old />女性
+                    <input type="radio" name="gender" value="other"old />その他
+                    <!--labelでくくるべき？-->
+                </div>
+                <div class="form__error">
+                    <!--バリデーション-->
+                </div>
+            </div>
+        </div>
+        <div class="form__group">
+            <div class="form__group-title">
+                <span class="form__label--item">メールアドレス</span>
+                <span class="form__label--required">※</span>
+            </div>
+            <div class="form__group-content">
+                <div class="form__input--text">
+                    <input type="email" name="email" placeholder="例：test@example.com" old/>
+                </div>
+                <div class="form__error">
+                    <!--バリデーション-->
+                </div>
+            </div>
+        </div>
+        <div class="form__group">
+            <div class="form__group-title">
+                <span class="form__label--item">電話番号</span>
+                <span class="form__label--required">※</span>
+            </div>
+            <div class="form__group-content">
+                <div class="form__input--text">
+                    <input type="text" name="tel1" placeholder="080" old />
+                    <input type="text" name="tel2" placeholder="1234" old />
+                    <input type="text" name="tel3" placeholder="5678" old />
+                </div>
+                <div class="form__error">
+                    <!--バリデーション-->
+                </div>
+            </div>
+        </div>
+        <div class="form__group">
+            <div class="form__group-title">
+                <span class="form__label--item">住所</span>
+                <span class="form__label--required">※</span>
+            </div>
+            <div class="form__group-content">
+                <div class="form__input--text">
+                    <input type="text" name="address" placeholder="例：東京都渋谷区千駄ヶ谷1-2-3" old/>
+                </div>
+                <div class="form__error">
+                    <!--バリデーション-->
+                </div>
+            </div>
+        </div>
+        <div class="form__group">
+            <div class="form__group-title">
+                <span class="form__label--item">建物名</span>
+            </div>
+            <div class="form__group-content">
+                <div class="form__input--text">
+                    <input type="text" name="building" placeholder="例：千駄ヶ谷マンション101" old/>
+                </div>
+                <div class="form__error">
+                    <!--バリデーション-->
+                </div>
+            </div>
+        </div>
+        <div class="form__group">
+            <div class="form__group-title">
+                <span class="form__label--item">お問い合わせの種類</span>
+                <span class="form__label--required">※</span>
+            </div>
+            <div class="form__group-content">
+                <div class="form__input--select">
+                    <select class="contact-form__select" name="contact_type">
+                        <option value="">選択してください</option>
+                        <option value="1">商品のお届けについて</option>
+                        <option value="2">商品の交換について</option>
+                        <option value="3">商品トラブル</option>
+                        <option value="4">ショップへのお問い合わせ</option>
+                    </select>
+                </div>
+                <div class="form__error">
+                    <!--バリデーション-->
+                </div>
+            </div>
+        </div>
+        <div class="form__group">
+            <div class="form__group-title">
+                <span class="form__label--item">お問い合わせ内容</span>
+                <span class="form__label--required">※</span>
+            </div>
+            <div class="form__group-content">
+                <div class="form__input--textarea">
+                <textarea name="content" placeholder="お問い合わせ内容をご記載ください"></textarea>
+                </div>
+                <div class="form__error">
+                    <!--バリデーション-->
+                </div>
+            </div>
+        </div>
+        <div class="form__button">
+            <button class="form__button-submit" type="submit">確認画面</button>
+        </div>
+    </form>
+</div>
+@endsection
